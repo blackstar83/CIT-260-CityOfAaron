549cdb9c071e5a1d4ceb97a54b359971a37cc507
blackstar83 <blackstar83@users.noreply.github.com>
2018-10-10 12:29:07 -0500

    Team Assignment - Game Class

---
+++ cityOfAaron/src/model/Map.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author gj3593
+ */
+public class Map {
+    
+    public Map(){
+        //Empty constructor for JavaBeans
+    }
+    
+    @Override
+    public String toString(){
+        return "Map class coming soon";
+    }
+}
---
+++ cityOfAaron/src/model/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author gj3593
+ */
+public class Player {
+    
+    public Player(){
+        //Empty constructor for JavaBeans
+    }
+    
+    @Override
+    public String toString(){
+        return "Player class coming soon";
+    }
+}
