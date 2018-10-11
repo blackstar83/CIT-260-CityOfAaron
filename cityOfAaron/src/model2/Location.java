bf2729eca307b5d63a32a4d81ceea1f18b3ffe01
apere <apere@DESKTOP-CQ1P4IU>
2018-10-10 22:33:23 -0700

    check please

---
+++ cityOfAaron/src/model2/Location.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model2;
+
+/**
+ *
+ * @author apere
+ */
+public class Location {
+
+   public Location() {
+       // empty constructor for JavaBeans
+   }
+   
+   @Override
+   public String toString() {
+       return "Location class coming soon";
+   }
+}
