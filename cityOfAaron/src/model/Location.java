d5bf19e1321242fa7504254b68d3bc1465d9f08b
apere <apere@DESKTOP-CQ1P4IU>
2018-10-11 22:08:01 -0700

    check please

---
+++ cityOfAaron/src/model/Location.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+public class Location implements Serializable{
+    private String name;
+    private String description;
+    private String mapSymbol;
+    private String[] gameTips;
+    
+    public Location() {
+}
+
+        public void setName(String name){
+            this.name = name;
+    }
+        public String getName() {
+            return name;
+        }
+        public void setDescription(String description){
+            this.description = description;
+        }
+        public String getDescription(){
+            return description;
+        }
+        public void setMapSymbol(){
+            this.mapSymbol = mapSymbol;
+        }
+        public String getMapSymbol(){
+            return mapSymbol;
+        }
+        public void setGameTips(){
+            this.gameTips = gameTips;
+        }
+        public String[] getGameTips(){
+            return gameTips;
+        }
+}
