2bba691f8b796fba67a8c5a8f7936e7a45e8a49f
myazz22 <yaz15001@byui.edu>
2018-10-11 19:44:53 -0700

    Individual Assignment - Player Class

---
+++ cityOfAaron/src/model3/InventoryItem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model3;
+
+/**
+ *
+ * @author gj3593
+ */
+public class InventoryItem {
+    
+    public InventoryItem(){
+        //Empty constructor for JavaBean
+    }
+    
+    @Override
+    public String toString(){
+        return "InventoryItem class coming soon";
+    }
+}
