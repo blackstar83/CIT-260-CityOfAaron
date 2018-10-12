f4d6cf217211a213140e595f2f671ce4e5f6b92c
myazz22 <yaz15001@byui.edu>
2018-10-12 00:12:48 -0700

    Individual Assignment - Player Class

--- cityOfAaron/src/model/Player.java
+++ cityOfAaron/src/model/Player.java
@@ -15,8 +15,8 @@ public class Player implements Serializable{
     
     private String name;
     
-    public Player(String name){
-        this.name = name;
+    public Player(){
+        
     }
     
     public String getName() {
