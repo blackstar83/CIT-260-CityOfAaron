604e511b3ecc56f93ae723d78e019565ec6ea047
gj3593 <gj3593@GGJ92N2.klec.klecasino.com>
2018-10-12 21:27:44 -0500

    Individual Assignment - Storehouse Class

---
+++ cityOfAaron/src/model/InventoryItem.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+/**
+ *
+ * @author gj3593
+ */
+public class InventoryItem implements Serializable{
+    private ItemType itemType;
+    private int quantity;
+    private Condition condition;
+    
+    public InventoryItem(){
+        //Empty constructor for JavaBean
+    }
+    
+    public ItemType getItemType() {
+        return itemType;
+    }
+    public void setItemType(ItemType type) {
+        this.itemType = itemType;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public Condition getCondition() {
+        return condition;
+    }
+    
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+}
