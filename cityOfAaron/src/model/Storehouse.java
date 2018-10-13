270c3d12bb5fecca59249ada36d3edd063f3f583
gj3593 <gj3593@GGJ92N2.klec.klecasino.com>
2018-10-12 21:36:13 -0500

    Individual Assignment - Storehouse Class

--- cityOfAaron/src/model/Storehouse.java
+++ cityOfAaron/src/model/Storehouse.java
@@ -5,18 +5,50 @@
  */
 package model;
 
+import java.io.Serializable;
 /**
  *
  * @author gj3593
  */
-public class Storehouse {
+public class Storehouse implements Serializable {
+    private Author[] authors;
+    private Animal[] animals;
+    private InventoryItem[] tools;
+    private Provision[] provisions;
     
     public Storehouse(){
         //Empty constructor for JavaBeans
     }
     
-    @Override
-    public String toString(){
-        return "Storehouse class coming soon";
+    public Author[] getAuthors() {
+        return authors;
+    }
+    
+    public void setAuthors (Author[] authors) {
+        this.authors = authors;
+    }
+    
+    public Animal[] getAnimals() {
+        return animals;
+    }
+    
+    public void setAnimals (Animal[] animals) {
+        this.animals = animals;
+    }
+    
+    public InventoryItem[] getInventoryItems() {
+        return tools;
+    }
+    
+    public void setInventoryItems (InventoryItem[] tools) {
+        this.tools = tools;
+    }
+    
+    public Provision[] getProvisions() {
+        return provisions;
+    }
+    
+    public void setProvisions(Provision[] provisions) {
+        this.provisions = provisions;
     }
 }
