60d6a1885ecf56c245ce083a59a1c204c9af91ca
gj3593 <gj3593@GGJ92N2.klec.klecasino.com>
2018-10-24 00:05:51 -0500

    Lesson 6 Team Assignment

--- cityOfAaron/Test Packages [Java]/control/GetRandomNumberTests.java
+++ cityOfAaron/Test Packages [Java]/control/GetRandomNumberTests.java
@@ -5,10 +5,59 @@
  */
 package control;
 
-/**
- *
- * @author gj3593
- */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class GetRandomNumberTests {
-    
+
+    @Test
+    public void InputsNotNegativeTest() {
+        int random = GameControl.getRandomNumber(-1, 10);
+        assertEquals(-1, random);
+
+        random = GameControl.getRandomNumber(10, -1);
+        assertEquals(-1, random);
+
+        random = GameControl.getRandomNumber(0, Integer.MAX_VALUE);
+        assertEquals(-3, random);
+    }
+
+    @Test
+    public void InputOrderTest() {
+        int random = GameControl.getRandomNumber(100, 10);
+        assertEquals(-2, random);
+
+        random = GameControl.getRandomNumber(5, 5);
+        assertEquals(-2, random);
+    }
+
+    @Test
+    public void MaxIntegerTest() {
+        int random = GameControl.getRandomNumber(0, Integer.MAX_VALUE);
+        assertEquals(-3, random);
+    }
+
+    @Test
+    public void ValidRangeTest() {
+        int[][] testRanges = new int[][]{
+            {1, 10},
+            {15, 100},
+            {5, 6},
+            {0, Integer.MAX_VALUE - 1}
+        };
+
+        for (int i = 0; i < testRanges.length; i++) {
+            int low = testRanges[i][0];
+            int high = testRanges[i][1];
+
+            int random = GameControl.getRandomNumber(low, high);
+            System.out.printf("%d <= %d <= %d\n", low, random, high);
+            assertTrue(
+                    String.format("%d should be between %d and %d", random, low, high),
+                    random >= low && random <= high);
+            assertTrue(
+                    String.format("%d should be between %d and %d", random, low, high),
+                    random >= low && random <= high);
+        }
+    }
 }
--- cityOfAaron/src/control/GameControl.java
+++ cityOfAaron/src/control/GameControl.java
@@ -5,36 +5,61 @@
  */
 package control;
 
+import java.util.Random;
+
+import model.Game;
+
 /**
  *
  * @author gj3593
  */
 public class GameControl {
-    
- /**
- *Generates a random integer between lowValue and highValue, inclusive.
- *<ul>Requirements:
- *<li>lowValue and highValue must be positive integers (&gt;=0) (return -1)</li>
- *<li>highValue must be greater than lowValue (return -2)</li>
- *<li>highValue cannot be equal to the maximum value for integers (return -3)</li>
- *</ul>
- *@param lowValue
- *@param highValue
- *@return The random number
- */
-public static int getRandomNumber(int lowValue, int highValue){
 
-    // if low < 0 or high < 0 the return -1
+    private static Random randomGenerator = new Random();
 
-    // if high <= low then return -2
+    /**
+     * Protected setter for tests to inject a mock random object.
+     *
+     * @param random
+     */
+    protected static void setRandomGenerator(Random random) {
+        randomGenerator = random;
+    }
 
-    // if high is the maximum value for integers, then return -3
-    
-    // calculate the size of the range; add one so Random() includes high value
+    /**
+     * Generates a random integer between lowValue and highValue, inclusive.
+     * <ul>Requirements:
+     * <li>lowValue and highValue must be positive integers (&gt;=0) (return
+     * -1)</li>
+     * <li>highValue must be greater than lowValue (return -2)</li>
+     * <li>highValue cannot be equal to the maximum value for integers (return
+     * -3)</li>
+     * </ul>
+     *
+     * @param lowValue
+     * @param highValue
+     * @return The random number
+     */
+    public static int getRandomNumber(int lowValue, int highValue) {
 
-    // return low + random(range size)
+        // if low < 0 or high < 0 the return -1
+        if (lowValue < 0 || highValue < 0) {
+            return -1;
+        }
 
-    // for now, return some value that would not be returned in any case
-    return -100;
-}
+        // if high <= low then return -2
+        if (highValue <= lowValue) {
+            return -2;
+        }
+
+        // if high is the maximum value for integers, then return -3
+        if (highValue == Integer.MAX_VALUE) {
+            return -3;
+        }
+        // calculate the size of the range; add one so Random() includes high value
+        int range = (highValue - lowValue) + 1;
+
+        // return low + random(range size)
+        return lowValue + randomGenerator.nextInt(range);
+    }
 }
