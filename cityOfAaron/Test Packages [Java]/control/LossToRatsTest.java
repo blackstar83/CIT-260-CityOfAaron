39076b1135e0d6cf7d9d1d5af00064290d8f5be3
apere <apere@DESKTOP-CQ1P4IU>
2018-10-25 22:06:56 -0700

    calculateLossToRats

---
+++ cityOfAaron/Test Packages [Java]/control/LossToRatsTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ *
+ * @author apere
+ */
+public class LossToRatsTest {
+    
+    public LossToRatsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of caclculateLossToRats method, of class LossToRats.
+     */
+    @Test
+    public void testCaclculateLossToRats() {
+        System.out.println("caclculateLossToRats");
+        int tithesPercent = 0;
+        int wheatInStorage = 0;
+        int expResult = 0;
+        int result = WheatControl.caclculateHarvest(tithesPercent, wheatInStorage);
+        assertEquals(expResult, result);
+    }
+
+}
