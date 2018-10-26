8348b4f4236b683b4c69e5f671c657f278418d5e
gj3593 <gj3593@GGJ92N2.klec.klecasino.com>
2018-10-25 21:52:42 -0500

    Lesson 6 Individual Assignment

---
+++ cityOfAaron/Test Packages [Java]/control/WheatControlTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gj3593
+ */
+public class WheatControlTest {
+    
+    public WheatControlTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of caclculateHarvest method, of class WheatControl.
+     */
+    @Test
+    public void testCaclculateHarvest() {
+        System.out.println("caclculateHarvest");
+        int tithesPercent = 0;
+        int acresPlanted = 0;
+        int expResult = 0;
+        int result = WheatControl.caclculateHarvest(tithesPercent, acresPlanted);
+        assertEquals(expResult, result);
+    }
+    
+}
--- cityOfAaron/src/control/WheatControl.java
+++ cityOfAaron/src/control/WheatControl.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package control;
 
-/**
- *
- * @author gj3593
- */
+import control.GameControl;
+
 public class WheatControl {
-    
+
+    /**
+     * Calculate the amount of wheat harvested, based on the percentage of
+     * tithing paid. Assume that GameControl.getRandomNumber(low,high) is
+     * available to be called. private static int calculateHarvest(int
+     * tithesPercent int acresPlanted) {
+     *
+     * @param tithesPercent
+     * @param acresPlanted
+     * @return The random number
+     */
+public static int caclculateHarvest(int tithesPercent, int acresPlanted) {
+        int low = 0, high = 0;
+        if (acresPlanted < 0) {
+            return -1;
+        }
+        if (tithesPercent < 0 || tithesPercent > 100) {
+            return -2;
+        }
+
+        if (tithesPercent < 8) {
+            low = 1;
+            high = 3;
+        }
+
+        if (tithesPercent >= 8 && tithesPercent <= 12) {
+
+             low = 2;
+             high = 4;
+        }
+
+        if (tithesPercent > 12) {
+
+             low = 2;
+             high = 5;
+        }
+        
+        int yield = GameControl.getRandomNumber(low, high);
+        return yield * acresPlanted;
+    }
 }
