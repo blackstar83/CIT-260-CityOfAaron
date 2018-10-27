1265d8feda56e0b655d14df977207091b1b10dc3
gj3593 <gj3593@GGJ92N2.klec.klecasino.com>
2018-10-26 22:56:03 -0500

    Lesson 6 Individual Assignment Second Try

--- cityOfAaron/Test Packages [Java]/control/WheatControlTest.java
+++ cityOfAaron/Test Packages [Java]/control/WheatControlTest.java
@@ -5,6 +5,9 @@
  */
 package control;
 
+import control.GameControl;
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -41,13 +44,33 @@ public class WheatControlTest {
      * Test of caclculateHarvest method, of class WheatControl.
      */
     @Test
-    public void testCaclculateHarvest() {
+    public void testCaclculateHarvest1() {
+        Random randomGenerator = new Random();
         System.out.println("caclculateHarvest");
-        int tithesPercent = 0;
-        int acresPlanted = 0;
-        int expResult = 0;
+        int tithesPercent = 99;
+        int acresPlanted = 1;
+        int expResult = 1 + randomGenerator.nextInt(5);
         int result = WheatControl.caclculateHarvest(tithesPercent, acresPlanted);
         assertEquals(expResult, result);
     }
-
+ @Test
+    public void testCaclculateHarvest2() {
+        Random randomGenerator = new Random();
+        System.out.println("caclculateHarvest");
+        int tithesPercent = 7;
+        int acresPlanted = 2;
+        int expResult = (1 + randomGenerator.nextInt(3)) * 2;
+        int result = WheatControl.caclculateHarvest(tithesPercent, acresPlanted);
+        assertEquals(expResult, result);
+    }
+ @Test
+    public void testCaclculateHarvest3() {
+        Random randomGenerator = new Random();
+        System.out.println("caclculateHarvest");
+        int tithesPercent = 8;
+        int acresPlanted = 4;
+        int expResult = (2 + randomGenerator.nextInt(3)) * 4;
+        int result = WheatControl.caclculateHarvest(tithesPercent, acresPlanted);
+        assertEquals(expResult, result);
+    }
 }
