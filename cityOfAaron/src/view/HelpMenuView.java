757859aa1f504fd1f77e5622dae13362dff89b58
gj3593 <gj3593@GGJ92N2.klec.klecasino.com>
2018-11-02 22:13:49 -0500

    Lesson 7 Final Team Assignment.

--- cityOfAaron/src/view/HelpMenuView.java
+++ cityOfAaron/src/view/HelpMenuView.java
@@ -1,4 +1,3 @@
-
 package view;
 
 import java.util.Scanner;
@@ -9,7 +8,7 @@ import view.*;
  * @author gj3593
  */
 public class HelpMenuView {
-    
+
     /**
      * The message that will be displayed by this view.
      */
@@ -27,7 +26,7 @@ public class HelpMenuView {
                 + "B. Where is the city of Aaron?\n"
                 + "C. How do I view the map?\n"
                 + "D. How do I move to another location?\n"
-                + "E. How do I display a list of animals, provisions and tools in the city storehouse?"
+                + "E. How do I display a list of animals, provisions and tools in the city storehouse?\n"
                 + "F. Back to the Main Menu.";
     }
 
@@ -101,22 +100,31 @@ public class HelpMenuView {
 
         switch (inputs[0].trim().toUpperCase()) {
             case "A":
-                System.out.println("TODO What are the goals of the game.....?");
+                System.out.println("You will assume the role of the leader over the city of Aaron. Wheat is the\n"
+                        + "staff of life, and is used as the main currency in the city. As ruler over the city, the your task\n"
+                        + "is to manage the village’s wheat crops so that the people of the village can be adequately fed,\n"
+                        + "while dealing with rats, and random crop yields. The city is blessed when the people pay their\n"
+                        + "tithes and offerings. After serving for 10 years, you will be judged by the people. If too\n"
+                        + "many people die during the your term of office, you will be removed from office and the game ends.");
                 break;
             case "B":
-                System.out.println("TODO Where is the city of Aaron.....?");
+                System.out.println("The City Of Aaron is in vicinity of Ammonihah, near cities of Moroni and Nephihah.");
                 break;
             case "C":
-                System.out.println("TODO How do I view the map....?");
+                System.out.println("The Map can be accessed in the Game Menu by selecting View The Map.\n"
+                        + "The map will contain a 2-dimensional array of locations.\n"
+                        + "Each location has a name, a description of what you can see at this location");
                 break;
             case "D":
-                System.out.println("TODO How do I move to another location.....?");
+                System.out.println("To move to a new location go to the Game Menu and select Move To A New Location\n"
+                        + "You will be prompted to enter the coordinates of the location on the map that you want to\n"
+                        + "move to.");
                 break;
             case "E":
-                System.out.println("TODO How do I display a list of animals, provisions and tools in the city storehouse?");
+                System.out.println("A list of animals, provisions and tools in the city storehouse can be viewed in the\n "
+                        + "Reports Menu by selecting View The Provisions In The Storehouse.");
                 break;
             case "F":
-                mainMenu();
                 return false;
         }
 
