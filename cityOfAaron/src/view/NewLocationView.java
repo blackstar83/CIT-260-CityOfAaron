169bc2db094436c0dcd0878b3fd91eb41ce1532f
gj3593 <gj3593@GGJ92N2.klec.klecasino.com>
2018-11-23 23:17:53 -0600

    Lesson 10 Corrections

--- cityOfAaron/src/view/NewLocationView.java
+++ cityOfAaron/src/view/NewLocationView.java
@@ -3,87 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package view;
 
 import java.util.Scanner;
+import view.*;
 
 /**
  *
  * @DYazz
  */
-public class NewLocationView {
+public class NewLocationView extends ViewBase {
 
-    /**
-     * The message that will be displayed by this view.
-     */
-    protected String message;
-
-    /**
-     * Constructor
-     */
     public NewLocationView() {
 
-        message = "Welcome to the Map.\n"
-              
+    }
+
+    @Override
+    protected String getMessage() {
+        return "Welcome to the Map.\n"
                 + "L - Type L to move to a new location.\n";
-                
-              
-               
-
     }
 
-    /**
-     * Get the user's input. Keep prompting them until they enter a value.
-     *
-     * @param prompt
-     * @param allowEmpty - determine whether the user can enter no value (just a
-     * return key)
-     * @return
-     */
-    protected String getUserInput(String prompt, boolean allowEmpty) {
-
-        Scanner keyboard = new Scanner(System.in);
-        String input = "";
-        boolean inputReceived = false;
-
-        while (inputReceived == false) {
-
-            System.out.println(prompt);
-            input = keyboard.nextLine();
-
-            // Make sure we avoid a null-pointer error.
-            if (input == null) {
-                input = "";
-            }
-
-            // Trim any trailing whitespace, including the carriage return.
-            input = input.trim();
-
-            if (input.equals("") == false || allowEmpty == true) {
-                inputReceived = true;
-            }
-        }
-
-        return input;
-    }
-
-    /**
-     * An overloaded version of getUserInput that sets allowEmpty to false so we
-     * don't have to type it ourselves.
-     *
-     * @param prompt
-     * @return
-     */
-    protected String getUserInput(String prompt) {
-        return getUserInput(prompt, false);
-    }
-
-    /**
-     * Get the set of inputs from the user.
-     *
-     * @return
-     */
+    @Override
     public String[] getInputs() {
 
         // Declare the array to have the number of elements you intend to get 
@@ -103,56 +44,21 @@ public class NewLocationView {
      * @return true if the view should repeat itself, and false if the view
      * should exit and return to the previous view.
      */
+    @Override
     public boolean doAction(String[] inputs) {
-        // Act on the user's input.
-        // This is a "dispatch" function that decides what
-        // other functions to call. You can use an if-, if-else,
-        // or switch statement.
-
-        // return false if you want this view to exit and return
-        // to the view that called it.
-        someActionHandler();
-
-        return true;
-    }
-
-    /**
-     * Control this view's display/prompt/action loop until the user chooses and
-     * action that causes this view to close.
-     */
-    public void displayView() {
-
-        boolean keepGoing = true;
-
-        while (keepGoing == true) {
-
-            System.out.println(message);
-            String[] inputs = getInputs();
-            keepGoing = doAction(inputs);
+        switch (inputs[0].trim().toUpperCase()) {
+            case "L":
+                startMapView();
+                return false;
         }
-    }
-
-    // Define your action handlers here. These are the methods that your doAction()
-    // method will call based on the user's input. We don't want to do a lot of 
-    // complex game stuff in our doAction() method. It will get messy very quickly.
-    private boolean someActionHandler() {
-        // Define whatever code you need here to accomplish the action.
-        // You can make this a void method if you want. Whatever you need 
-        // here, you are free to do.
-        //
-        // Generally, though, this is where you will call into your Control
-        // classes to do the work of the application.
 
         return true;
     }
-        private void newLocationView() {
-        //newLocation view = new newLocationView();
-        //view.displayView();
-        System.out.println("Implementation coming soon.");
+
+    private void startMapView() {
+
+        MapView view = new MapView();
+        view.displayView();
     }
 
-    
 }
-
-    
-
